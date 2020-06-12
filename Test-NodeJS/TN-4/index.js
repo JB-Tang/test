88ca9491b9586cd09baa3c5146c329ac9b537e22
JB-Tang <374079358@qq.com>
2020-06-12 12:42:06 +0800

    提交数据库连接相关代码

--- Test-NodeJS/TN-4/index.js
+++ Test-NodeJS/TN-4/index.js
@@ -108,10 +108,126 @@
 /**
  * 测试连接数据库
  */
-const express=require('express')
-const mysql=require('mysql');
-const cors=require('cors');
-const bodyParser=require('body-parser');  //解析参数
+// const express=require('express')
+// const mysql=require('mysql');
+// const cors=require('cors');
+// const bodyParser=require('body-parser');  //解析参数
+// const app=express();
+// const router=express.Router();
+// const option={
+//     host:'localhost',
+//     user:'root',
+//     password:'xxxxxxxxx',
+//     port:'3306',
+//     database:'test',
+//     connectTimeout:5000,    //连接超时
+//     multipleStatements:false,   //是否允许一个query中包含多条sql语句
+// }
+// app.use(cors());    //解决跨域
+// app.use(bodyParser.json()); //json请求
+// app.use(bodyParser.urlencoded({extended:false}));   //表单请求
+// app.listen(80,()=>{
+//     console.log("服务启动");
+// });
+
+// const conn=mysql.createConnection(option);
+// app.all('/login',(req,res)=>{
+//     conn.query('SELECT * FROM student',(e,r)=>{
+//         res.json(new Result(r))
+//     })
+// })
+
+// function Result(data){
+//     return {
+//         code:1,
+//         msg:'success!',
+//         data:data
+//     }
+// }
+
+/**
+ * 测试使用构造函数封装数据库连接
+ */
+// const express=require('express');
+// const mysql=require('mysql');
+// const cors=require('cors');
+// const bodyParser=require('body-parser');  //解析参数
+// const app=express();
+// const router=express.Router();
+// const option={
+//     host:'localhost',
+//     user:'root',
+//     password:'xxxxxxxx',
+//     port:'3306',
+//     database:'test',
+//     connectTimeout:5000,    //连接超时
+//     multipleStatements:false,   //是否允许一个query中包含多条sql语句
+// }
+// app.use(cors());  //解决跨域
+// app.use(bodyParser.json()); //json请求
+// app.use(bodyParser.urlencoded({extended:false}));   //表单请求
+// app.listen(80,()=>{
+//     console.log("服务启动");
+// })
+
+// var conn=mysql.createConnection(option);
+// app.all('/login',(req,res)=>{
+//     conn.connect();
+//     conn.query('SELECT * FROM student',(e,r)=>{
+//         res.json(r)
+//     });
+//     conn.end();
+// })
+
+/**
+ * 测试使用构造函数的重连机制
+ */
+// const express=require('express');
+// const mysql=require('mysql');
+// const cors=require("cors");
+// const bodyParser=require("body-parser");
+// const app=express();
+// const router=express.Router();
+// const option={
+//     host:'localhost',
+//     user:'root',
+//     password:'xxxxxxxxx',
+//     port:'3306',
+//     database:'test',
+//     connectTimeout:5000,    //连接超时
+//     multipleStatements:false,   //是否允许一个query中包含多条sql语句
+// }
+// app.use(cors());    //解决跨域
+// app.use(bodyParser.json());  //json请求
+// app.use(bodyParser.urlencoded({extended:false}));   //表单请求
+// app.listen(80,()=>{
+//     console.log("服务启动");
+// })
+
+// var conn;
+// reconn();
+
+// app.all('/login',(req,res)=>{
+//     conn.connect();
+//     conn.query('SELECT * FROM student',(e,r)=>{
+//         res.json(r)
+//     })
+//     // conn.end();  //此处如果添加end()的话，那么只能连接数据库一次
+// })
+
+// //断线重连机制
+// function reconn(){
+//     conn=mysql.createConnection(option);
+//     conn.on('error',err=>err.code==='PROTOCOL_CONNECTION_LOST' && setTimeout(reconn,2000))
+// }
+
+/**
+ * 测试使用数据库连接池
+ */
+const express=require("express");
+const mysql=require("mysql");
+const cors=require("cors");
+const bodyParser=require("body-parser");
 const app=express();
 const router=express.Router();
 const option={
@@ -122,25 +238,36 @@ const option={
     database:'test',
     connectTimeout:5000,    //连接超时
     multipleStatements:false,   //是否允许一个query中包含多条sql语句
+    waitForConnections:true,    //当无连接池可用时，等待（true）还是抛错（false）
+    connectionLimit:100,    //连接数限制
+    queueLimit:0,   //最大连接等待数（0为不限制）
 }
 app.use(cors());    //解决跨域
 app.use(bodyParser.json()); //json请求
 app.use(bodyParser.urlencoded({extended:false}));   //表单请求
 app.listen(80,()=>{
-    console.log("服务启动");
-});
+    console.log("服务启动")
+})
 
-const conn=mysql.createConnection(option);
+var pool
+repool()
+
 app.all('/login',(req,res)=>{
-    conn.query('SELECT * FROM student',(e,r)=>{
-        res.json(new Result(r))
+    // 方案一：直接使用
+    pool.query('SELECT * FROM student',(e,r)=>{
+        res.json(r)
+    })
+    //方案二：从连接池中取一个连接
+    pool.getConnection((err,conn)=>{
+        conn.query('SELECT * FROM student',(e,r)=>{
+            res.json(r)
+        })
+        conn.release();
     })
 })
 
-function Result(data){
-    return {
-        code:1,
-        msg:'success!',
-        data:data
-    }
+//断线重连机制
+function repool(){
+    pool=mysql.createPool(option); //创建连接池
+    pool.on('error',err=>err.code==='PROTOCOL_CONNECTION_LOST' && setTimeout(repool,2000))
 }
