09ec4c7d425419076e6301e0b66a68758aa1036c
JB-Tang <374079358@qq.com>
2020-06-17 17:12:16 +0800

    完善部分代码

--- Test-NodeJS/TN-7/routes/index.js
+++ Test-NodeJS/TN-7/routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let express=require('express');
+let router=express.Router();
+let getArticle=require('../controller/article/getArticle_controller');
+let addArticle=require('../controller/article/addArticle_controller');
+/**
+ * 数据查询
+ */
+router.get('/article/list/get',(req,res,next)=>{
+  getArticle(req.query,data=>{
+    res.end(JSON.stringify(data));
+  })
 });
-
-module.exports = router;
+/**
+ * 数据新增
+ * @type {Router|router}
+ * request.body.XX
+ */
+router.all('/article/list/add',(req,res,next)=>{
+  req.body={
+    name:'测试',
+    url:'http://www.test.aa',
+    alexa:'32',
+    country:'CN'
+  }
+  addArticle(req.body,data=>{
+    res.end(JSON.stringify(data));
+  })
+});
+module.exports=router;
