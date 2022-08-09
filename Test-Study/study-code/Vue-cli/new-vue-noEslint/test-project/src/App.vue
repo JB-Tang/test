9f7be98bf0b95a7a8b98eed93da11421cd324a3a
唐加兵 <374079358@qq.com>
2022-08-09 10:15:59 +0800

    创建一个没有eslint校验的vue2项目

---
+++ Test-Study/study-code/Vue-cli/new-vue-noEslint/test-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <TestEcharts/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TestEcharts from './pages/TestEchart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TestEcharts
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
